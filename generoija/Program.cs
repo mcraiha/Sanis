6d4a5187498200db89d84c8a2d2e7030f28f4493
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-03-31 21:30:15 +0300

    Paremmat ohjeet
    Jos joku muukin haluaa kokeilla

--- generoija/Program.cs
+++ generoija/Program.cs
@@ -28,7 +28,7 @@ namespace Generoija
 			{
 				if (args.Length != 3)
 				{
-					PrintHeadersHelp();
+					PrintTranslationsHelp();
 					return;
 				}
 
@@ -38,7 +38,7 @@ namespace Generoija
 			{
 				if (args.Length != 4)
 				{
-					PrintHeadersHelp();
+					PrintCreateJSONHelp();
 					return;
 				}
 
@@ -49,12 +49,20 @@ namespace Generoija
 
 		private static void PrintHeadersHelp()
 		{
-			Console.WriteLine("otsikot 100 tiedosto");
+			Console.WriteLine("otsikot haluttu_määrä tiedosto");
+			Console.WriteLine("esim: dotnet run otsikot 2000 fiwiktionary-20190320-pages-articles-multistream.xml");
 		}
 
 		private static void PrintTranslationsHelp()
 		{
-			Console.WriteLine("kaannokset 100 tiedosto");
+			Console.WriteLine("kaannokset haluttu_määrä tiedosto");
+			Console.WriteLine("esim: dotnet run kaannokset 100 fiwiktionary-20190320-pages-articles-multistream.xml");
+		}
+
+		private static void PrintCreateJSONHelp()
+		{
+			Console.WriteLine("teejson haluttu_määrä tiedosto_sisään tiedosto_ulos");
+			Console.WriteLine("esim: dotnet run teejson 100 fiwiktionary-20190320-pages-articles-multistream.xml tulos.json");
 		}
 	}
 }

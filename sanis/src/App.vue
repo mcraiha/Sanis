0919a8e5e0e99b6bf4f9ec5b36dee5eb6595dec1
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-04-07 14:34:07 +0300

    DevLog mukaan
    Jotta voi mitata suorituksen aikoja

--- sanis/src/App.vue
+++ sanis/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <DevLog v-bind:devLogs="devLog" />
     <TextInput v-bind:searchTerm.sync="searchTerm"/>
     <p>{{ searchTerm }}</p>
     <LanguagePairSelect />
@@ -12,19 +13,25 @@ import { Component, Vue } from 'vue-property-decorator';
 import TextInput from './components/TextInput.vue';
 import LanguagePairSelect from './components/LanguagePairSelect.vue';
 import ShowResults from './components/ShowResults.vue';
+import DevLog from './components/DevLog.vue';
 
 @Component({
   components: {
     TextInput,
     LanguagePairSelect,
     ShowResults,
+    DevLog
   },
   data: function () {
     return { 
       searchTerm: 'abs' as string, 
       closestMatches: ['ab', 'bb', 'bc'] as string[], 
       dataLoaded: false as boolean, 
-      dictionary: null as any 
+      dictionary: null as any,
+
+      // Development log
+      devLogEnabled: false as boolean,
+      devLog: ['Dev log start'] as string[], 
       }
   },
   methods: {
@@ -44,9 +51,16 @@ export default class App extends Vue {
   // Lifecycle hook
   async mounted ()
   {
+    const jsonHandleStartTime = performance.now();
+
     const response = await fetch('dictionaries/1-2.json');
     const data = await response.json();
     this.$data.dictionary = data;
+
+    const jsonHandleEndTime = performance.now();
+
+    this.$data.devLog.push(`json handling took: ${jsonHandleEndTime - jsonHandleStartTime} milliseconds`);
+
     // console.log(data);
     this.$data.dataLoaded = true;
     //this.$data.searchTerm = 'arebawebawbbaw';
---
+++ sanis/src/components/DevLog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="devLogs">
+    <ul id="example-1">
+        <li v-for="item in this.devLogs">
+            {{ item }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DevLog extends Vue {
+  @Prop() private devLogs!: string[];
+}
+</script>

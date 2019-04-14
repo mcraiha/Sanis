f52591794022e69534dbcba1a1706f6d1cbb3109
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-04-14 13:52:09 +0300

    Details-pohjainen tulosten esittely
    Ei kaunis, mutta toimii

--- sanis/src/components/ShowResults.vue
+++ sanis/src/components/ShowResults.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="showResults">
     <p>Hakusana: {{ exactSearchTerm }}</p>
-    <p>Osuma: {{ exactMatch }}</p>
-    <ul id="example-1">
-        <li v-for="item in this.closestMatches">
-            {{ item }}
-        </li>
-    </ul>
+    <p>Osuma: 
+      <details v-if="doesExactMatchContainSomething()" open>
+        <summary>{{ exactMatch.word }}</summary>
+        <p v-for="exactTranslation in this.exactMatch.translations">
+          {{ exactTranslation }}
+        </p>
+      </details>
+    </p>
+    <p>Sivuosumat:
+      <details v-for="item in this.closestMatches" open>
+        <summary>{{ item.word }}</summary>
+        <p v-for="translation in item.translations">
+          {{ translation }}
+        </p>
+      </details>
+    </p>
   </div>
 </template>
 
@@ -20,5 +30,10 @@ export default class ShowResults extends Vue {
   @Prop() private exactSearchTerm!: string;
   @Prop() private exactMatch!: IDictionaryEntry;
   @Prop() private closestMatches!: IDictionaryEntry[];
+
+  private doesExactMatchContainSomething() : boolean
+  {
+    return !(this.exactMatch.word == null || this.exactMatch.word.length < 1);
+  }
 }
 </script>

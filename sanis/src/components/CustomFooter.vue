64d569aeccd05364e9f867b2f3b2de6267469c49
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-05-02 15:58:43 +0300

    Show buildinfo in footer
    Easier to see what has changed

--- sanis/src/components/CustomFooter.vue
+++ sanis/src/components/CustomFooter.vue
@@ -6,14 +6,24 @@
         <div>
             <a href="tietoja.html">Tietoja</a>
         </div>
+        <div>
+            <small>{{ getBuildTime() }} {{ getGitCommit() }}</small>
+        </div>
     </footer>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import buildinfo from '../buildinfo.json';
 
 @Component
 export default class CustomFooter extends Vue {
+    private getBuildTime(): string {
+        return buildinfo.buildTime;
+    }
 
+    private getGitCommit(): string {
+        return buildinfo.gitCommit;
+    }
 }
 </script>
